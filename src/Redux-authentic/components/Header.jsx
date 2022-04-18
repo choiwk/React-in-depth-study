a2fbacc128ccbe42f8eec441423da1dd7efcc087
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-18 13:53:18 +0900

    ⚡️ 로그인 및 로그아웃 쿠키 존재 여부에 따라 Header의 정보 적용 및

--- src/Redux-authentic/components/Header.jsx
+++ src/Redux-authentic/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import { Grid, Text, Button } from '../elements/ImportBridge';
+import { getCookie, setCookie, deleteCookie } from '../../shared/Cookie';
 
 const Header = () => {
+  const [is_login, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    let cookie = getCookie('user_id');
+    console.log(cookie);
+    if (cookie) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, []);
+
   const locatedOnLogin = () => {
     document.location.href = '/login';
   };
@@ -10,8 +24,32 @@ const Header = () => {
   const locatedOnSignUp = () => {
     document.location.href = '/signup';
   };
+
+  if (is_login) {
+    return (
+      <HeaderContainer>
+        <Grid is_flex padding="4px 16px">
+          <Grid>
+            <Text margin="0px" size="24px" bold>
+              헬로
+            </Text>
+          </Grid>
+
+          <Grid is_flex>
+            <Button text="내정보" _onClick={locatedOnLogin}></Button>
+            <Button text="알림" _onClick={locatedOnSignUp}></Button>
+            <Button
+              text="로그아웃"
+              _onClick={() => deleteCookie('user_id')}
+            ></Button>
+          </Grid>
+        </Grid>
+      </HeaderContainer>
+    );
+  }
+
   return (
-    <React.Fragment>
+    <>
       <Grid is_flex padding="4px 16px">
         <Grid>
           <Text margin="0px" size="24px" bold>
@@ -24,8 +62,11 @@ const Header = () => {
           <Button text="회원가입" _onClick={locatedOnSignUp}></Button>
         </Grid>
       </Grid>
-    </React.Fragment>
+    </>
   );
 };
 
+const HeaderContainer = styled.header`
+  border: 1px solid red;
+`;
 export default Header;
