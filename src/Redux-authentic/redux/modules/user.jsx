6e78688371a277e43a3be2ac21ea9a1779823d8b
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-18 15:58:18 +0900

    🚧 유저 모듈 redux-actions와 immer 라이브러리로 셋팅 진행 및 produce()

--- src/Redux-authentic/redux/modules/user.jsx
+++ src/Redux-authentic/redux/modules/user.jsx
@@ -7,29 +7,20 @@ const LOG_OUT = 'LOG_OUT';
 const GET_USER = 'GET_USER';
 
 //action creators
-//TODO: redux-actions 라이브러리의 createAction을 사용하기 전.
-//? const logIn = (user) => {
-//?  return {
-//?   type: LOG_IN,
-//?     user,
-//?   };
-//? };
-
-//TODO: createAction을 사용해서 action 생성해주기.
 const logIn = createAction(LOG_IN, (user) => ({ user }));
+const logOut = createAction(LOG_OUT, (user) => ({ user }));
+const getUser = createAction(GET_USER, (user) => ({ user }));
 
-//!
-//TODO: redux-actions 라이브러리의 handleActions 사용하기 전.
-//? const reducer = (state = {}, action = {}) => {
-//?   switch (action.type) {
-//?     case LOG_IN:
-//?       state.user = action.user;
-//?     case LOG_OUT:
-//?       return;
-//?     case GET_USER:
-//?       return;
-//?   }
-//? };
+//initialState = default props
+const initialState = {
+  user: null, //항상 처음 로그인 상태는 없음 상태.
+  is_login: false, //웹사이트 뜨자마자 로그인 상태 false
+};
 
-//TODO: handleActions을 사용해서 reducer 생성해주기.
-const reducer = handleActions({ [LOG_IN]: (state, action) => {} }, {});
+//reducer
+export default handleActions({
+  [LOG_IN]: (state, action) => produce(state, (draft) => {}),
+  //TODO: reducer안에서 일어나는 작업의 불변성 유지 작업 = immer의 produce()
+  [LOG_OUT]: (state, action) => {},
+  [GET_USER]: (state, action) => {},
+});
