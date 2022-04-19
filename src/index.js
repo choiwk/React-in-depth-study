db7085a957aa119c5fd399e21fd73cfee81201b6
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-19 21:09:57 +0900

    ⚡️ 최상위 컴포넌트 App에 Provider으로 store 연결하기

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './shared/App';
+import { Provider } from 'react-redux';
+
+import store from './Redux-authentic/redux/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
