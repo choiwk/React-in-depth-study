cbb53d0365c64f01738954e5220d5fe23d0442f9
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-19 21:12:28 +0900

    🐛  내가 만든 history를 App.jsx에서 사용하기 위해 ConnectedRouter로 연결

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -5,16 +5,18 @@ import PostList from '../Redux-authentic/pages/PostList';
 import SignUp from '../Redux-authentic/pages/SignUp';
 import Login from '../Redux-authentic/pages/Login';
 import { Grid } from '../Redux-authentic/elements/ImportBridge';
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from '../Redux-authentic/redux/configureStore';
 
 function App() {
   return (
     <React.Fragment>
       <Grid>
-        <BrowserRouter>
+        <ConnectedRouter history={history}>
           <Route path="/" exact component={PostList} />
           <Route path="/signup" exact component={SignUp} />
           <Route path="/login" exact component={Login} />
-        </BrowserRouter>
+        </ConnectedRouter>
       </Grid>
     </React.Fragment>
   );
