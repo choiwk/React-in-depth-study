fc465ab228fe1021261d13c55d189d29c51754b2
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-22 14:23:10 +0900

    ⚡️ firebase SDK 설치 및 Firebase 앱 객체 생성하기.

---
+++ src/shared/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkdVw-jsP5i7WV9vBv6zgReZ2mer1R3rk',
+  authDomain: 'sparta-react-f7584.firebaseapp.com',
+  projectId: 'sparta-react-f7584',
+  storageBucket: 'sparta-react-f7584.appspot.com',
+  messagingSenderId: '725975879849',
+  appId: '1:725975879849:web:190867194ad8a90d6b99b8',
+  measurementId: 'G-HPCR98G1H1',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export { auth };
