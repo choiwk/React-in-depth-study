a1a76ea05b65884296f48a8a422a4e384c825570
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-23 15:22:32 +0900

    middlewares 코드 주석 설명 추가.

--- src/Redux-authentic/redux/configureStore.jsx
+++ src/Redux-authentic/redux/configureStore.jsx
@@ -18,7 +18,7 @@ const rootReducer = combineReducers({
 //, redux-thunk라이브러리를 이용해 내가 사용하고 싶은 미들웨어를 모두 넣으려는 곳.
 //, reducer을 실행하기 전에 action이 실행되고 reducer의 실행 전 사이에 middlewares의 thunk를 쓰면 비동기를 동작할 수 있는 단계를 설정할 수 있다.
 //' 실행 : action -> 비동기 작업(외부 api작업 다녀옴, setTimeOut() 등. ) middlewares -> reducer을 태움.
-const middlewares = [thunk.withExtraArgument({ history: history })];
+const middlewares = [thunk.withExtraArgument({ history: history })]; //? 로그인후 특정 페이지로 이동 = history
 //, history로 움직이는 것도 middlewares thunk에서 한다.
 //' withExtraArgument : thunk에 내장되어있는 함수. -> 다른 인수를 더 넘겨주도록 함.
 //* 요약 : action 생성실행됨 -> 비동기 다녀오고 .then으로 history를 받아서 사용 가능 -> reudcer() 실행됨.
@@ -31,7 +31,7 @@ if (env === 'development') {
   const { logger } = require('redux-logger'); // require()은 import보다 정보 공유도가 더 낮은 패키지를 가져올때 사용.
   middlewares.push(logger); // [thunk, logger...]
 }
-
+ 
 //! redux-devtool
 const composeEnhancers =
   // 지금 환경이 브라우저가 아닐때도 있다. 브라우저가 아니더라도 Javascript 엔진은 동작한다, 브라우저가 아닐때는 window라는 객체가 없기 때문에
@@ -43,6 +43,7 @@ const composeEnhancers =
     : compose;
 
 //! 미들웨어(middlewares) 묶기
+// 스토어에 미들웨어를 적용 할 때에는 applyMiddleware 라는 함수를 사용합니다.
 // redux라이브러리의 ApplyMiddleware()로 지금까지 있는 middlewares를 사용하겠다 라는 설정으로 composeEnhancers()로 한번 묶어줘서 사용해야한다.
 const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
