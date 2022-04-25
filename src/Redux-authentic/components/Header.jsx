dc3aa4dd55d36c71d144835eb7fde7fd5e38187b
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-25 17:45:15 +0900

    ⚡️ location.href로 페이지 이동 대신 만들어놓은 history로 페이지 이동하기.

--- src/Redux-authentic/components/Header.jsx
+++ src/Redux-authentic/components/Header.jsx
@@ -4,18 +4,11 @@ import { Grid, Text, Button } from '../elements/ImportBridge';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { actionCreators as userActions } from '../redux/modules/user';
+import { history } from '../redux/configureStore';
 
 const Header = () => {
   const dispatch = useDispatch();
   const is_login = useSelector((state) => state.user.is_login);
-
-  const locatedOnLogin = () => {
-    document.location.href = '/login';
-  };
-
-  const locatedOnSignUp = () => {
-    document.location.href = '/signup';
-  };
 
   if (is_login) {
     return (
@@ -52,8 +45,14 @@ const Header = () => {
         </Grid>
 
         <Grid is_flex>
-          <Button text="로그인" _onClick={locatedOnLogin}></Button>
-          <Button text="회원가입" _onClick={locatedOnSignUp}></Button>
+          <Button
+            text="로그인"
+            _onClick={() => history.push('/login')}
+          ></Button>
+          <Button
+            text="회원가입"
+            _onClick={() => history.push('/signup')}
+          ></Button>
         </Grid>
       </Grid>
     </>
