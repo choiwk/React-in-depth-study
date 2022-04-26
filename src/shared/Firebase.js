98b7bd75905ce0f7783c0e1058707c3ed65e0056
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-26 17:04:56 +0900

    🚧 apiKey export 하기. Header.jsx에서 session_key를 사용하기 위함.

--- src/shared/Firebase.js
+++ src/shared/Firebase.js
@@ -13,5 +13,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+const apiKey = firebaseConfig.apiKey;
 
-export { auth };
+export { auth, apiKey };
