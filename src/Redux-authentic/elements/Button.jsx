161f8b2526d63440a3d9197714d274b1b3b9bc79
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-28 16:41:44 +0900

    ⚡️ float fixed속성 button 생성.

--- src/Redux-authentic/elements/Button.jsx
+++ src/Redux-authentic/elements/Button.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const Button = (props) => {
-  const { text, _onClick } = props;
+  const { text, _onClick, is_float } = props;
+
+  if (is_float) {
+    return (
+      <>
+        <FloatButton onClick={_onClick}>{text}</FloatButton>
+      </>
+    );
+  }
+
   return (
     <React.Fragment>
       <ElButton onClick={_onClick}>{text}</ElButton>
@@ -14,7 +23,7 @@ const Button = (props) => {
 Button.defaultProps = {
   text: '',
   _onClick: () => {},
-  bg: '#000000',
+  is_float: false,
 };
 
 const ElButton = styled.button.attrs({
@@ -23,10 +32,29 @@ const ElButton = styled.button.attrs({
 })`
   width: 100%;
   background-color: #212121;
-  color: #ffffff;
+  color: #fff;
   padding: 15px 0px;
   box-sizing: border-box;
   border: none;
+  cursor: pointer;
+`;
+
+const FloatButton = styled.button.attrs({
+  type: 'button',
+  alt: '기능 동작 버튼',
+})`
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  right: 20%;
+  bottom: 30px;
+  background-color: #ffa500;
+  color: #fff;
+  box-sizing: border-box;
+  font-size: 30px;
+  font-weight: 600;
+  border: none;
+  border-radius: 50%;
   cursor: pointer;
 `;
 
