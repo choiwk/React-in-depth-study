ecb029f9d013f68239574fbc59aada0f1f3c4980
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-28 22:17:59 +0900

    🚧 useSelector로 post reducer안에 initialState 값 사용하기, props 전달.

--- src/Redux-authentic/pages/PostList.jsx
+++ src/Redux-authentic/pages/PostList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Post from '../components/Post';
+import { useSelector } from 'react-redux';
 
 const PostList = () => {
+  const post_list = useSelector((state) => state.post.list);
+  console.log(post_list);
   return (
     <>
       <Post />
+      {post_list.map((postItem, idx) => (
+        <Post key={idx} {...postItem} />
+      ))}
     </>
   );
 };
