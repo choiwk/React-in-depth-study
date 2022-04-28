2f73660d3bd4c470a6dd45bb458da9c09c079550
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-28 21:45:57 +0900

    🚧 포스트(post) Redux 초기 셋팅하기.

---
+++ src/Redux-authentic/redux/modules/post.js
@@ -0,0 +1,41 @@
+import { createActions, handleActions } from 'redux-actions';
+import { produce } from 'immer';
+
+const SET_POST = 'SET_POST';
+const ADD_POST = 'ADD_POST';
+
+const setPost = createActions(SET_POST, (post_list) => ({ post_list }));
+const addPost = createActions(ADD_POST, (post) => ({ post }));
+
+const initialState = {
+  list: [],
+};
+
+const initialPost = {
+  id: 0,
+  user_info: {
+    user_name: 'wonkeun',
+    user_profile:
+      'https://www.notion.so/f7be487bc3aa4842aaab051497fba7cf#500e914360c046c1b1b68c2b26142f0f',
+  },
+  image_url:
+    'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160103_119%2Fyukkie_14518066357879h3ul_JPEG%2FIMG_1752.jpg&type=sc960_832',
+  contents: '괌 여행 가고싶다',
+  comment_cnt: 10,
+  insert_dt: '2021-12-29 15:00:00',
+};
+
+export default handleActions(
+  {
+    [SET_POST]: (state, action) => produce(state, (draft) => {}),
+    [ADD_POST]: (state, action) => produce(state, (draft) => {}),
+  },
+  initialState
+);
+
+const actionCreators = {
+  setPost,
+  addPost,
+};
+
+export { actionCreators };
