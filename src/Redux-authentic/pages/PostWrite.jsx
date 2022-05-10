cc91493f53a0940ed3de91021dc36764cd0339f3
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-05-10 16:35:22 +0900

    🚧 게시물 작성 컴포넌트 구현하기.

--- src/Redux-authentic/pages/PostWrite.jsx
+++ src/Redux-authentic/pages/PostWrite.jsx
@@ -1,7 +1,35 @@
-import { Grid, Text, Button, Image, Input } from '../elements';
+import React from 'react';
+import { Grid, Text, Button, Image, Input } from '../elements/ImportBridge';
+import { useSelector, useDispatch } from 'react-redux';
+import { history } from '../redux/configureStore';
 
 const PostWrite = () => {
-  <Grid padding="16px"></Grid>;
+  console.log(history);
+  const is_login = useSelector((state) => state.user.is_login);
+
+  if (!is_login) {
+    return (
+      <Grid margin="100px 0" padding="16px" center>
+        <Text size="32px" bold>
+          알림!
+        </Text>
+        <Text size="16px">로그인 후에만 글을 작성할 수 있습니다.</Text>
+        <Button
+          text="로그인 하러가기"
+          _onClick={() => {
+            history.replace('/');
+          }}
+        ></Button>
+      </Grid>
+    );
+  }
+  return (
+    <>
+      <Grid padding="16px">
+        <Text margin="0" size="36px" bold></Text>
+      </Grid>
+    </>
+  );
 };
 
 export default PostWrite;
