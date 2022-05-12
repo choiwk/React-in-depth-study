1f64c0352dcfa23bd09e65c7dae9537b61e15dfd
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-05-12 16:34:50 +0900

    🚧 firestore에 선택한 이미지 파일 저장하기.

--- src/shared/Upload.jsx
+++ src/shared/Upload.jsx
@@ -1,25 +1,27 @@
 import React, { useRef } from 'react';
 import { Button } from '../Redux-authentic/elements/ImportBridge';
+import { storage } from './Firebase';
 
 const Upload = () => {
   const fileInput = useRef();
 
   const uploadFB = () => {
     let image = fileInput.current.files[0];
-    console.log(image);
+    const _upload = storage.ref(`images/${image.name}`).put(image);
+
+    _upload.then((doc) => {
+      console.log(doc);
+    });
   };
 
   const changeFile = (e) => {
-    console.log(e);
-    console.log(e.target);
-
     console.log(e.target.files);
     console.log(fileInput.current.files[0]);
   };
   return (
     <>
       <input type="file" onChange={changeFile} ref={fileInput} />
-      <Button _onClick={uploadFB}></Button>
+      <Button _onClick={uploadFB} text="업로드하기"></Button>
     </>
   );
 };
