d999145652d170559b5a34ad0d195b42a99f302e
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-05-20 18:21:54 +0900

    🚧 알림 컴포넌트에 자식 컴포넌트 Card 호출 및 임의 noti 객체 생성.

--- src/Redux-authentic/pages/Notification.jsx
+++ src/Redux-authentic/pages/Notification.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Grid, Text } from '../elements';
+import { Grid, Text } from '../elements/ImportBridge';
+import Card from '../components/Card';
 
-const Notification = () => {
-  return <></>;
+const Notification = (props) => {
+  console.log(props);
+
+  let noti = [
+    { user_name: 'mean0', post_id: 'post1' },
+    { user_name: 'mean0', post_id: 'post2' },
+    { user_name: 'mean0', post_id: 'post3' },
+    { user_name: 'mean0', post_id: 'post4' },
+  ];
+  return (
+    <>
+      <Grid padding="16px" bg="#EFF6FF">
+        {noti.map((n) => {
+          return <Card {...n} key={n.post_id} />;
+        })}
+      </Grid>
+    </>
+  );
 };
 
 export default Notification;
