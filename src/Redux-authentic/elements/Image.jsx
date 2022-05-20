045f87d47e474d128b49dccfe1c41cc344df5991
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-05-20 18:18:53 +0900

    ⚡️이미지 없을때의 스타일 설정 defaultImage

--- src/Redux-authentic/elements/Image.jsx
+++ src/Redux-authentic/elements/Image.jsx
@@ -21,8 +21,20 @@ const Image = (props) => {
     );
   }
 
-  return <React.Fragment></React.Fragment>;
+  return (
+    <>
+      <ImageDefault {...styles}></ImageDefault>
+    </>
+  );
 };
+
+const ImageDefault = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  background-image: url('${(props) => props.src}');
+  background-size: cover;
+`;
 
 const ImageCircle = styled.div`
   --size: ${(props) => props.size}px;
