3fd4cee958cc835c9276857376607b3945a440ca
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-05-24 19:11:50 +0900

    🚧  무한스크롤 준비 : paging 구현 진행중- start, next 값 store에 전달.

--- src/Redux-authentic/redux/modules/post.js
+++ src/Redux-authentic/redux/modules/post.js
@@ -7,6 +7,7 @@ import moment from 'moment';
 const SET_POST = 'SET_POST';
 const ADD_POST = 'ADD_POST';
 const EDIT_POST = 'EDIT_POST';
+const LOADING = 'LOADING';
 
 const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
@@ -14,9 +15,12 @@ const editPost = createAction(EDIT_POST, (post_id, post) => ({
   post_id,
   post,
 }));
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
 const initialState = {
   list: [],
+  paging: { start: null, next: null, size: 3 },
+  is_loading: false,
 };
 
 const initialPost = {
@@ -139,33 +143,54 @@ const addPostFB = (contents = '') => {
   };
 };
 
-const getPostFB = () => {
+const getPostFB = (start = null, size = 3) => {
   return function (dispatch, getState, { history }) {
     const postDB = firestore.collection('post');
 
-    let query = postDB.orderBy('insert_dt', 'desc').limit(3);
-    query.get().then((docs) => {
-      let post_list = [];
-      docs.forEach((docElements) => {
-        let _post = docElements.data();
-        let post = Object.keys(_post).reduce(
-          (acc, cur) => {
-            if (cur.indexOf('user_') !== -1) {
-              return {
-                ...acc,
-                user_info: { ...acc.user_info, [cur]: _post[cur] },
-              };
+    dispatch(loading(true));
+    let query = postDB.orderBy('insert_dt', 'desc');
+    if (start) {
+      query = query.startAt(start);
+    }
+    query
+      .limit(size + 1)
+      .get()
+      .then((docs) => {
+        let post_list = [];
+
+        let paging = {
+          start: docs.docs[0],
+          next:
+            docs.docs.length === size + 1
+              ? docs.docs[docs.docs.length - 1]
+              : null,
+          size: size,
+        };
+
+        docs.forEach((docElements) => {
+          console.log(docElements);
+          let _post = docElements.data();
+          let post = Object.keys(_post).reduce(
+            (acc, cur) => {
+              if (cur.indexOf('user_') !== -1) {
+                return {
+                  ...acc,
+                  user_info: { ...acc.user_info, [cur]: _post[cur] },
+                };
+              }
+              return { ...acc, [cur]: _post[cur] };
+            },
+            {
+              id: docElements.id,
+              user_info: {},
             }
-            return { ...acc, [cur]: _post[cur] };
-          },
-          {
-            id: docElements.id,
-            user_info: {},
-          }
-        );
-        post_list.push(post);
+          );
+          post_list.push(post);
+        });
+        post_list.pop();
+        console.log(post_list);
+        dispatch(setPost(post_list, paging));
       });
-    });
   };
 };
 
@@ -188,6 +213,10 @@ export default handleActions(
           ...draft.list[idx],
           ...action.payload.post,
         };
+      }),
+    [LOADING]: (state, action) =>
+      produce(state, (draft) => {
+        draft.is_loading = action.payload.is_loading;
       }),
   },
   initialState
