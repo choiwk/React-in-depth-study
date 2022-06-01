9074f74b4349c37d388462f4e63d458a2ee80dfb
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-06-01 20:47:43 +0900

    ⚡️ 상세페이지 새로고침시 오류 - 파이어스토어에서 데이터 단일로 호출하여 state로 관리해주기.

--- src/Redux-authentic/pages/PostDetail.jsx
+++ src/Redux-authentic/pages/PostDetail.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from '../components/Post';
 import CommentWrite from '../components/CommentWrite';
 import CommentList from '../components/CommentList';
 import { useSelector } from 'react-redux';
+import { firestore } from '../../shared/Firebase';
 
 const PostDetail = (props) => {
   const id = props.match.params.id;
+
   const user_info = useSelector((state) => state.user.user);
+
   const post_list = useSelector((store) => store.post.list);
-  console.log('post_list : ', post_list);
-  const post_idx = post_list.findIndex((el) => el.id === id);
-  console.log('post_idx : ', post_idx);
-  const post = post_list[post_idx];
-  console.log('post : ', post);
-  console.log(user_info);
+
+  const post_idx = post_list.findIndex((p) => p.id === id);
+  const post_data = post_list[post_idx];
+
+  const [post, setPost] = React.useState(post_data ? post_data : null);
+
+  React.useEffect(() => {
+    if (post) {
+      return;
+    }
+
+    const postDB = firestore.collection('post');
+    postDB
+      .doc(id)
+      .get()
+      .then((doc) => {
+        console.log(doc);
+        console.log(doc.data());
+
+        let _post = doc.data();
+        let post = Object.keys(_post).reduce(
+          (acc, cur) => {
+            if (cur.indexOf('user_') !== -1) {
+              return {
+                ...acc,
+                user_info: { ...acc.user_info, [cur]: _post[cur] },
+              };
+            }
+            return { ...acc, [cur]: _post[cur] };
+          },
+          { id: doc.id, user_info: {} }
+        );
+
+        setPost(post);
+      });
+  }, []);
 
   return (
-    <>
-      <Post {...post} is_me={post.user_info.user_id === user_info.uid} />
+    <React.Fragment>
+      {post && (
+        <Post {...post} is_me={post.user_info.user_id === user_info.uid} />
+      )}
       <CommentWrite />
       <CommentList />
-    </>
+    </React.Fragment>
   );
 };
 
