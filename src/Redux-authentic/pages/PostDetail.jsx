b031baeca2b317870b35fe180f878c22c239ceb1
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-06-01 19:25:46 +0900

    🚧 게시글 상세페이지로 이동 연결하기.

--- src/Redux-authentic/pages/PostDetail.jsx
+++ src/Redux-authentic/pages/PostDetail.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-
 import Post from '../components/Post';
 import CommentWrite from '../components/CommentWrite';
 import CommentList from '../components/CommentList';
+import { useSelector } from 'react-redux';
 
-const PostDetail = () => {
+const PostDetail = (props) => {
+  const id = props.match.params.id;
+  const user_info = useSelector((state) => state.user.user);
+  const post_list = useSelector((store) => store.post.list);
+  console.log('post_list : ', post_list);
+  const post_idx = post_list.findIndex((el) => el.id === id);
+  console.log('post_idx : ', post_idx);
+  const post = post_list[post_idx];
+  console.log('post : ', post);
+  console.log(user_info);
+
   return (
     <>
-      <Post />
+      <Post {...post} is_me={post.user_info.user_id === user_info.uid} />
       <CommentWrite />
       <CommentList />
     </>
