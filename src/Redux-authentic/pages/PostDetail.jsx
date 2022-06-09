51d7d01bec9cfce7e84dddf7ee193b4e35c08758
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-06-09 19:01:38 +0900

    리덕스로 단일 정보 가져오는 함수 - 상세페이지 클릭시 파이어스토어에서 게시글 하나 정보 가져오기

--- src/Redux-authentic/pages/PostDetail.jsx
+++ src/Redux-authentic/pages/PostDetail.jsx
@@ -2,72 +2,48 @@ import React, { useState, useEffect } from 'react';
 import Post from '../components/Post';
 import CommentWrite from '../components/CommentWrite';
 import CommentList from '../components/CommentList';
-import { useSelector } from 'react-redux';
-import { firestore } from '../../shared/Firebase';
-import { history } from '../redux/configureStore';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionCreators as postActions } from '../redux/modules/post';
 
 const PostDetail = (props) => {
+  const dispatch = useDispatch();
   const id = props.match.params.id;
 
   const login = useSelector((state) => state.user.is_login);
   const user_info = useSelector((state) => state.user.user);
   const post_list = useSelector((store) => store.post.list);
 
-  const post_idx = post_list.findIndex((p) => p.id === id);
+  const post_idx = post_list.findIndex((el) => el.id === id);
   const post_data = post_list[post_idx];
 
-  console.log('✅', post_data);
-
-  const [post, setPost] = useState(post_data ? post_data : null);
-
   const accessDenied = () => {
-    alert('로그인을 하셔야 게시글을 확인할 수 있습니다.');
-    history.push('/login');
+    console.log('Ehd!');
+
+    // alert('로그인이 필요한 페이지입니다.');
+    // history.push('/login');
   };
 
   useEffect(() => {
-    if (post) {
+    if (post_data) {
       return;
     }
-
-    const postDB = firestore.collection('post');
-
-    postDB
-      .doc(id)
-      .get()
-      .then((doc) => {
-        let _post = doc.data();
-        let post = Object.keys(_post).reduce(
-          (acc, cur) => {
-            if (cur.indexOf('user_') !== -1) {
-              return {
-                ...acc,
-                user_info: { ...acc.user_info, [cur]: _post[cur] },
-              };
-            }
-            return { ...acc, [cur]: _post[cur] };
-          },
-          { id: doc.id, user_info: {} }
-        );
-
-        setPost(post);
-      });
+    dispatch(postActions.getOnePostFB(id));
   }, []);
 
   return (
     <React.Fragment>
-      {post && (
+      {post_data && (
         <Post
-          {...post}
+          {...post_data}
           is_me={
-            login === false
-              ? accessDenied()
-              : post.user_info.user_id === user_info.uid
+            login
+              ? post_data.user_info.user_id === user_info.uid
+              : post_data.user_info.user_id === null
           }
         />
       )}
-      <CommentWrite />
-      <CommentList />
+      <CommentWrite post_id={id} />
+      <CommentList post_id={id} />
     </React.Fragment>
   );
 };
