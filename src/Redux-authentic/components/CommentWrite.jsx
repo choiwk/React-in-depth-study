b3f02e6293ffaa2d0e80fc5d5fe2067d40f9303a
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-06-12 19:47:22 +0900

    🚧 입력한 댓글 내용 리덕스 store에 저장하기

--- src/Redux-authentic/components/CommentWrite.jsx
+++ src/Redux-authentic/components/CommentWrite.jsx
@@ -4,17 +4,23 @@ import { Grid, Input, Text, Button } from '../elements/ImportBridge';
 import { useSelector, useDispatch } from 'react-redux';
 import { actionCreators as commentActions } from '../redux/modules/comment';
 
-const CommentWrite = () => {
+const CommentWrite = (props) => {
   const dispatch = useDispatch();
+  const { post_id } = props;
   const [commentText, setCommentText] = useState('');
 
   const commentTarget = (e) => {
     setCommentText(e.target.value);
   };
 
-  const targetValue = () => {
-    console.log(commentText);
-    dispatch(commentActions.addCommentFB());
+  const write = () => {
+    if (commentText === '') {
+      window.alert('댓글을 입력해주세요!');
+      return;
+    }
+    // 파이어스토어에 추가합니다.
+    dispatch(commentActions.addCommentFB(post_id, commentText));
+    // 입력된 텍스트는 지우기!
     setCommentText('');
   };
 
@@ -24,12 +30,13 @@ const CommentWrite = () => {
         placeholder="댓글 내용을 입력해주세요."
         _onChange={commentTarget}
         value={commentText}
+        is_submit
       />
       <Button
         width="20%"
         text="작성하기"
         padding="13px 0"
-        _onClick={targetValue}
+        _onClick={write}
       ></Button>
     </Grid>
   );
